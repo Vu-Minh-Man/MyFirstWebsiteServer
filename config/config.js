19809d1f347a7473e40774c8799c9a5d6ff46748
Vu Minh Man <vuminhmanmlm@gmail.com>
2022-03-14 17:33:06 +0700

    change port.

--- config/config.js
+++ config/config.js
@@ -1,8 +1,7 @@
 const db = "mongodb+srv://man:EhOpohrI0hAGcnR5@cluster0.kjz7v.mongodb.net";
-const host = "https://sleepy-ocean-50803.herokuapp.com";
 
 export default {
-  port: host || 8080,
+  port: process.env.PORT || 8080,
   dbDiscussion: db + "/myFirstWebsite",
   dbLogger: db + "/errors",
   secretKey: process.env.SECRETKEY || "jwtkey",
