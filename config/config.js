fcc86d48eb2d1907bd945fcea584e2b1573bd3ae
Vu Minh Man <vuminhmanmlm@gmail.com>
2022-03-15 12:02:51 +0700

    Change URI to URL.

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 const dbCluster =
-  process.env.MONGODB_URI ||
+  process.env.MONGODB_URL ||
   "mongodb+srv://man:EhOpohrI0hAGcnR5@cluster0.kjz7v.mongodb.net";
 
 const dbParams = "?retryWrites=true&w=majority";
