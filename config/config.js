4aec09855a68936da221316d72132c375bc1e08b
Vu Minh Man <vuminhmanmlm@gmail.com>
2022-03-15 11:31:32 +0700

    cm

--- config/config.js
+++ config/config.js
@@ -1,9 +1,10 @@
-const dbCluster =
-  "mongodb+srv://man:EhOpohrI0hAGcnR5@cluster0.kjz7v.mongodb.net";
+// const dbCluster =
+//   "mongodb+srv://man:EhOpohrI0hAGcnR5@cluster0.kjz7v.mongodb.net";
 // process.env.MONGODB_URI
+
 export default {
   port: process.env.PORT || 8080,
-  db: dbCluster + "/myFirstWebsite?retryWrites=true&w=majority",
-  dbLogger: dbCluster + "/errors?retryWrites=true&w=majority",
+  db: process.env.MONGODB_URI + "/myFirstWebsite?retryWrites=true&w=majority",
+  dbLogger: process.env.MONGODB_URI + "/errors?retryWrites=true&w=majority",
   secretKey: process.env.SECRETKEY || "jwtkey",
 };
