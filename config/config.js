4ceace91408c0c7b3763f4a99091e58e5f044807
Vu Minh Man <vuminhmanmlm@gmail.com>
2022-03-17 08:53:04 +0700

    Remove MongoDB URL leak.

--- config/config.js
+++ config/config.js
@@ -1,12 +1,8 @@
-const dbCluster =
-  process.env.MONGODB_URL ||
-  "mongodb+srv://man:EhOpohrI0hAGcnR5@cluster0.kjz7v.mongodb.net";
-
 const dbParams = "?retryWrites=true&w=majority";
 
 export default {
   port: process.env.PORT || 8080,
-  db: dbCluster + "/myFirstWebsite" + dbParams,
-  dbLogger: dbCluster + "/errors" + dbParams,
+  db: process.env.MONGODB_URL + "/myFirstWebsite" + dbParams,
+  dbLogger: process.env.MONGODB_URL + "/errors" + dbParams,
   secretKey: process.env.SECRETKEY || "jwtkey",
 };
